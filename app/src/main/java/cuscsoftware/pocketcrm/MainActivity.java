a7ffb0c1b24dab4805b138a0190ee406635ef379
Huy <lthuy@ctu.edu.vn>
2017-07-02 17:05:43 +0900

    click item, map

--- app/src/main/java/cuscsoftware/pocketcrm/MainActivity.java
+++ app/src/main/java/cuscsoftware/pocketcrm/MainActivity.java
@@ -1,9 +1,11 @@
 package cuscsoftware.pocketcrm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.CursorAdapter;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,7 +15,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -60,6 +64,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        lvCustomer.setOnItemClickListener(new CustomerList());
     }
 
     @Override
@@ -111,11 +116,22 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
-
+            Intent inten = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(inten);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+}
+
+class CustomerList implements AdapterView.OnItemClickListener{
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i("MyDEBUG", "Clicked");
     }
 }
--- app/src/main/java/cuscsoftware/pocketcrm/MapsActivity.java
+++ app/src/main/java/cuscsoftware/pocketcrm/MapsActivity.java
@@ -39,8 +39,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng sydney = new LatLng(10.033893, 105.779888);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in CUSC"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+
+
     }
 }
